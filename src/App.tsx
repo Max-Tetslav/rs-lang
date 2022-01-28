29d9d5f3dd8800510a38ab3a657ce2f9096f71a0
Max Teclav <maks.tetslav@yandex.ru>
2022-01-28 06:41:18 +0300

    feat: update app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React, { Fragment } from 'react';
 
+import Header from './components/layout/header/Header';
+import Menu from './components/layout/menu/Menu';
+import Homepage from './views/homepage/Homepage';
+
 function App() {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <Menu/>
+      <Header title='Главная'/>
+      <Homepage/>
+    </Fragment>
   );
 }
 
