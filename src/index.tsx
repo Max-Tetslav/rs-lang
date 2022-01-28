9cd75c26913f297786f5c48e7d2c46139300b7a8
Max Teclav <maks.tetslav@yandex.ru>
2022-01-28 06:41:58 +0300

    refactor: update index file

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.body);
+const root: HTMLElement = document.getElementById('app-root') as HTMLElement;
+
+ReactDOM.render(<App />, root);
