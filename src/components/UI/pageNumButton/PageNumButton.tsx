cfc298cce44fbbddb1ed2d4efb8e803f82b231ff
Max Teclav <maks.tetslav@yandex.ru>
2022-02-10 17:01:30 +0300

    feat: implement pageNumButton component

---
+++ src/components/UI/pageNumButton/PageNumButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './PageNumButton.module.scss';
+
+function PageNumButton() {
+  return (
+    <button className={cl.button} type="button">
+      1
+    </button>
+  );
+}
+
+export default PageNumButton;
