3faa45c5909b737c193ef39d40985ac2e37e4958
Max Teclav <maks.tetslav@yandex.ru>
2022-02-15 14:57:13 +0300

    feat: implement hardUnitMenuItem component

--- src/components/UI/hardUnitMenuItem/HardUnitMenuItem.tsx
+++ src/components/UI/hardUnitMenuItem/HardUnitMenuItem.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useAppSelector } from '../../../utils/helpers/appHooks';
 import cl from './HardUnitMenuItem.module.scss';
 
-function HardUnitMenuItem(): JSX.Element {
+interface IProps {
+  setIsHardUnit: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function HardUnitMenuItem({ setIsHardUnit }: IProps): JSX.Element {
+  const isAuth = useAppSelector((state) => state.users.loggedIn);
+
   return (
-    <button className={cl.button} type="button">
+    <button
+      className={cl.button}
+      type="button"
+      onClick={() => {
+        setIsHardUnit(true);
+      }}
+      disabled={!isAuth}
+    >
       Сложные слова
     </button>
   );
