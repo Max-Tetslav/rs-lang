39c1b897e0d034d54708ffaf776aa62dda59bdb2
Max Teclav <maks.tetslav@yandex.ru>
2022-02-15 15:05:16 +0300

    feat: implement store stuff for user's hardWords

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './usersReducer';
+import wordsReducer from './words';
 
-const rootReducer = combineReducers({ users: usersReducer });
+const rootReducer = combineReducers({
+  users: usersReducer,
+  words: wordsReducer,
+});
 export default rootReducer;
