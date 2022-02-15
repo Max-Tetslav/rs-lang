3b0b45f98836afb08aa7c3789d322ddd3c2b473b
Max Teclav <maks.tetslav@yandex.ru>
2022-02-15 14:54:40 +0300

    feat: implement unitMenuItem component

--- src/components/UI/unitMenuItem/UnitMenuItem.tsx
+++ src/components/UI/unitMenuItem/UnitMenuItem.tsx
@@ -4,11 +4,21 @@ import cl from './UnitMenuItem.module.scss';
 interface IProps {
   unitNum: string;
   setUnit: React.Dispatch<React.SetStateAction<number>>;
+  setPageNum: React.Dispatch<React.SetStateAction<number>>;
+  resetHardUnit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function UnitMenuItem({ unitNum, setUnit }: IProps): JSX.Element {
+function UnitMenuItem({ unitNum, setUnit, setPageNum, resetHardUnit }: IProps): JSX.Element {
   return (
-    <button className={cl.button} onClick={() => setUnit(Number(unitNum) - 1)} type="button">
+    <button
+      className={cl.button}
+      type="button"
+      onClick={() => {
+        resetHardUnit(false);
+        setPageNum(0);
+        setUnit(Number(unitNum) - 1);
+      }}
+    >
       {unitNum ? `Часть ${unitNum}` : 'Сложные слова'}
     </button>
   );
