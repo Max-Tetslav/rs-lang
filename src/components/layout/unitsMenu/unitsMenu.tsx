ccf6c820c8aebdca13f1d2d8c19f77c7c9886d00
Max Teclav <maks.tetslav@yandex.ru>
2022-02-15 14:55:35 +0300

    feat: implement unitsMenu component

--- src/components/layout/unitsMenu/unitsMenu.tsx
+++ src/components/layout/unitsMenu/unitsMenu.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import cl from './unitsMenu.module.scss';
 
 import UnitMenuItem from '../../UI/unitMenuItem/UnitMenuItem';
+import HardUnitMenuItem from '../../UI/hardUnitMenuItem/HardUnitMenuItem';
 
 interface IProps {
   setCurrentUnit: React.Dispatch<React.SetStateAction<number>>;
+  changePage: React.Dispatch<React.SetStateAction<number>>;
+  setHardUnit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function UnitsMenu({ setCurrentUnit }: IProps): JSX.Element {
+function UnitsMenu({ setCurrentUnit, changePage, setHardUnit }: IProps): JSX.Element {
   return (
     <div className={cl.container}>
-      <UnitMenuItem unitNum="1" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="2" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="3" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="4" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="5" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="6" setUnit={setCurrentUnit} />
-      <UnitMenuItem unitNum="" setUnit={setCurrentUnit} />
+      <UnitMenuItem unitNum="1" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <UnitMenuItem unitNum="2" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <UnitMenuItem unitNum="3" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <UnitMenuItem unitNum="4" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <UnitMenuItem unitNum="5" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <UnitMenuItem unitNum="6" setUnit={setCurrentUnit} setPageNum={changePage} resetHardUnit={setHardUnit} />
+      <HardUnitMenuItem setIsHardUnit={setHardUnit} />
     </div>
   );
 }
