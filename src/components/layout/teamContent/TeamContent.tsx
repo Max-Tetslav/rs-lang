ebd506a3f6da3ef982ff08cbed4e74d100767d84
Nadya <snotra.notra@gmail.com>
2022-02-21 10:49:22 +0300

    feat: add team page

--- src/components/layout/teamContent/TeamContent.tsx
+++ src/components/layout/teamContent/TeamContent.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import cl from './TeamContent.module.scss';
 import TEAM_DATA from '../../../utils/constants/teamConstants';
-import team from '../../../assets/images/team.jpg';
+import githubLogo from '../../../assets/images/github-logo.png';
 
 function TeamContent() {
   return (
     <div className={cl.container}>
-      <div className={cl.img}>
-        <img src={team} alt="" />
-      </div>
       <div className={cl.team}>
         {TEAM_DATA.map(({ name, github, image, role, desc }) => {
           return (
             <div className={cl.team__item}>
               <div className={cl.img}>
-                <a className={cl.img__link} href={github}>
-                  <img className={cl.img__img} src={image} alt="" />
-                </a>
+                <img className={cl.img__img} src={image} alt="" />
               </div>
               <div className={cl.content}>
-                <div className={cl.name}>{name}</div>
+                <div className={cl.name}>
+                  {name}
+                  <a className={cl.github} href={github} target="_blank" rel="noreferrer">
+                    <img src={githubLogo} alt="" />
+                  </a>
+                </div>
                 <div className={cl.role}>{role}</div>
                 <div className={cl.desc}>{desc}</div>
               </div>
