60e49cf41f9ff6e439a39d2d62e236ad050bd0bc
Max Teclav <maks.tetslav@yandex.ru>
2022-02-21 20:40:29 +0300

    feat: improve pagination

--- src/components/layout/pagination/Pagination.tsx
+++ src/components/layout/pagination/Pagination.tsx
@@ -1,20 +1,64 @@
+// код взят и адаптирован отсюда https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
+
 import React from 'react';
+import { usePagination, DOTS } from '../../../hooks/usePagination';
 import cl from './Pagination.module.scss';
-import PageNumButton from '../../UI/pageNumButton/PageNumButton';
 
 interface IProps {
-  setPage: React.Dispatch<React.SetStateAction<number>>;
+  onPageChange: React.Dispatch<React.SetStateAction<number>>;
+  totalCount: number;
+  siblingCount: number;
+  currentPage: number;
+  pageSize: number;
 }
 
-const pagesNum = Array.from(Array(30).keys()).map((item) => item + 1);
+function Pagination({ onPageChange, totalCount, siblingCount = 2, currentPage, pageSize }: IProps): JSX.Element {
+  const paginationRange = usePagination(totalCount, pageSize, siblingCount, currentPage);
+  const lastPage = (paginationRange as (number | string)[])[(paginationRange as (number | string)[]).length - 1];
 
-function Pagination({ setPage }: IProps): JSX.Element {
+  function onNext() {
+    onPageChange(currentPage - 1 + 1);
+  }
+
+  function onPrevious() {
+    onPageChange(currentPage - 1 - 1);
+  }
+
   return (
-    <div className={cl.container}>
-      {pagesNum.map((item) => {
-        return <PageNumButton key={item} pageNum={item} clickHandler={setPage} />;
+    <ul className={[cl.paginationContainer, cl.paginationBar].join(' ')}>
+      <li
+        className={currentPage === 1 ? [cl.paginationItem, cl.disabled].join(' ') : [cl.paginationItem].join(' ')}
+        onClick={onPrevious}
+        onKeyPress={() => {}}
+        role="menuitem"
+      >
+        <div className={[cl.arrow, cl.left].join(' ')} />
+      </li>
+      {(paginationRange as (number | string)[]).map((pageNumber) => {
+        if (pageNumber === DOTS) {
+          return <li className={[cl.paginationItem, cl.dots].join(' ')}>&#8230;</li>;
+        }
+
+        return (
+          <li
+            className={pageNumber === currentPage ? [cl.paginationItem, cl.selected].join(' ') : [cl.paginationItem].join(' ')}
+            onClick={() => onPageChange(Number(pageNumber) - 1)}
+            onKeyPress={() => {}}
+            role="menuitem"
+          >
+            {pageNumber}
+          </li>
+        );
       })}
-    </div>
+      <li
+        className={currentPage === lastPage ? [cl.paginationItem, cl.disabled].join(' ') : [cl.paginationItem].join(' ')}
+        onClick={onNext}
+        onKeyPress={() => {}}
+        role="menuitem"
+      >
+        <div className={[cl.arrow, cl.right].join(' ')} />
+      </li>
+    </ul>
   );
 }
 
